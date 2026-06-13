ffd67a3450530102c79036e3ea103b0470fcdd98
ManikantaBurla <burlamanikantasai@gmail.com>
2026-06-13 23:18:46 +0530

    Update DB config

--- backend/config/db.php
+++ backend/config/db.php
@@ -1,14 +1,14 @@
 <?php
+// This tells PHP to grab the specific values you just saved in Vercel
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$name = getenv('DB_NAME');
+$port = getenv('DB_PORT');
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$database = "WorldStream";
-
-$conn = mysqli_connect($host, $user, $password, $database);
+$conn = mysqli_connect($host, $user, $pass, $name, $port);
 
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 ?>
