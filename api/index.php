9d6dcd94d5b919f2fba898f08ede78f2a6940096
ManikantaBurla <burlamanikantasai@gmail.com>
2026-06-14 09:16:16 +0530

    Fix redirect loop in index.php to allow login page access

--- api/index.php
+++ api/index.php
@@ -1,10 +1,16 @@
 <?php
 session_start();
 
+// Check if user is logged in
 if (isset($_SESSION['user'])) {
-    header("Location: /frontend/pages/home.php"); // Use relative path
+    // If already logged in and trying to go to login, send to home
+    header("Location: /frontend/pages/home.php");
 } else {
-    header("Location: /frontend/pages/login.php"); // Use relative path
+    // If not logged in, send to login
+    // IMPORTANT: Make sure we aren't already on the login page to avoid the loop!
+    if ($_SERVER['REQUEST_URI'] !== '/frontend/pages/login.php') {
+        header("Location: /frontend/pages/login.php");
+    }
 }
-exit;
+exit();
 ?>
